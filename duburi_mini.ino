98ca184ee0cbb08040db6eb139a229db90a2fae0
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-02-17 17:32:27 +0600

    Update duburi_mini.ino

--- duburi_mini.ino
+++ duburi_mini.ino
@@ -1,4 +1,17 @@
 
+int motor1pin1 = 2;
+int motor1pin2 = 3;
+int motor2pin1 = 4;
+int motor2pin2 = 5;
+
+
+int motor1pin1 = 8;
+int motor1pin2 = 9;
+int motor2pin1 = 10;
+int motor2pin2 = 11;
+
+
+
 /// stored for catching arduino input 
 void input_template(){ 
     char input;
