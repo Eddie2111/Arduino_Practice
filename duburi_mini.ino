39601d536fc0c97c0ad7a40a6de1b7137a884a02
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-02-17 16:59:06 +0600

    Update duburi_mini.ino

--- duburi_mini.ino
+++ duburi_mini.ino
@@ -27,10 +27,10 @@ void setup(){
     pinMode(5,OUTPUT);
     
     //motor driver 2
-    pinMode(6,OUTPUT);
-    pinMode(7,OUTPUT);
     pinMode(8,OUTPUT);
     pinMode(9,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(11,OUTPUT);
     
     //for others
 }
